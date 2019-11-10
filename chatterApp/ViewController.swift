95af9812a7b0ebf3dcef715237bcfc5ea276c3ad
karan <karan.sagar1028@gmail.com>
2019-11-10 17:46:32 +0530

    Initial Commit

---
+++ chatterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatterApp
+//
+//  Created by Karan Sagar on 10/11/19.
+//  Copyright © 2019 Karan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
